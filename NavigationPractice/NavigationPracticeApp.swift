327dd4e997b00f24c8c790613a9525566fa788d9
Emily Suriel <emilysuriel0@gmail.com>
2025-07-10 10:01:19 -0400

    Initial Commit

---
+++ NavigationPractice/NavigationPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPracticeApp.swift
+//  NavigationPractice
+//
+//  Created by Indiana Suriel on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
